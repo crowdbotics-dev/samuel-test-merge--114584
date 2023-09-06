272df19ca935c8a32161349dd9b88dc6166c545c
Crowdbotics <engineering@crowdbotics.com>
2023-09-06 07:16:35 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "samuel-test-merge--114584",
+  url: "https://samuel-test-merge--114584.botics.co",
+  api: "https://samuel-test-merge--114584.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
